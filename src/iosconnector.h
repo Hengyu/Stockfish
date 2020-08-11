17e4213bbc4b50d64360066c396b68368246720a
hengyu <hengyu@outlook.com>
2020-08-11 20:45:35 +0800

    Xcode framework support

---
+++ src/iosconnector.h
@@ -0,0 +1,13 @@
+#ifndef IOSEXTENSION_H
+#define IOSEXTENSION_H
+
+#include <vector>
+
+#include "types.h"
+
+extern void principal_variation(std::vector<Move> moves, const Value& score, int depth, uint64_t nodes, long long time);
+extern void searched_move(const Move& currmove, unsigned long currmovenum, int depth);
+extern void best_move(const Move& best, const Move& ponder, const Value& score, const Score& contempt);
+extern void position_is_draw(void);
+
+#endif // !defined(IPHONE_H_INCLUDED)
