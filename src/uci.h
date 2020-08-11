65abd6fa986e34c1a21f03d13b9d03d24e209046
hengyu <hengyu@outlook.com>
2020-08-11 20:46:17 +0800

    Support for SPM

--- src/uci.h
+++ src/uci.h
@@ -76,6 +76,9 @@ std::string pv(const Position& pos, Depth depth, Value alpha, Value beta);
 std::string wdl(Value v, int ply);
 Move to_move(const Position& pos, std::string& str);
 
+// PUSH iOS
+void execute_command(const char *str);
+// POP iOS
 } // namespace UCI
 
 extern UCI::OptionsMap Options;
